ab768a21c0ea11800390657988b851fc94976e68
regitapramesti1 <cahyaniregita623@gmail.com>
2024-05-17 23:24:34 +0700

    add paragraph in Beranda.js

--- screens/Beranda.js
+++ screens/Beranda.js
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const Beranda = () => {
+  const navigation = useNavigation();
+
+        useLayoutEffect(() => {
+            navigation.setOptions({
+                headerShown:false,
+            });
+          },[]);
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <View className="flex-row px-6 mt-8 items-center justify-between px-8">
+      <View className="flex-row px-3 mt-6 items-center space-x-1">
+                <Text className="text-[#67C6E3] text-5xl font-bold">News</Text>
+            <View>
+                <Text className="text-black text-5xl font-bold">Nexus</Text>
+            </View>
+        </View>
+      </View>
+    </SafeAreaView>
+      
+    
   )
 }
 
