1be7f3bc62c23aa88aa287e078504e6fe837029b
regitapramesti1 <cahyaniregita623@gmail.com>
2024-05-22 01:31:34 +0700

    add paragraph in Beranda.js add file MenuContainer.js add folder components add paragraph MenuContainer.js add line in index.js

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,7 @@
 export { default as reporter } from "./reporter.png";
+export { default as all } from "./all.png";
+export { default as sports } from "./sports.png";
+export { default as health } from "./health.png";
+export { default as travel } from "./travel.png";
+export { default as edu } from "./edu.png";
+export { default as foods } from "./foods.png";
--- screens/Beranda.js
+++ screens/Beranda.js
@@ -1,16 +1,27 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
-import { Avatar } from '../assets';
+import MenuContainer from '../components/MenuContainer';
+import { all, sports, health } from '../assets';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+
 
 const Beranda = () => {
   const navigation = useNavigation();
+  const [type, setType] = useState('');
 
         useLayoutEffect(() => {
             navigation.setOptions({
                 headerShown:false,
             });
           },[]);
+
+          const menuItems = [
+            { key: "all", title: "all", imageSrc: all },
+            { key: "sports", title: "sports", imageSrc: sports },
+            { key: "health", title: "health", imageSrc: health }
+          ];
 
   return (
     <SafeAreaView className="flex-1 bg-white relative">
@@ -23,6 +34,33 @@ const Beranda = () => {
 
         </View>
       </View>
+      {/*Search Section */}
+      <View className="flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 shadow-lg mt-4">
+       
+      </View>
+      {/*Menu Section */}
+      <ScrollView>
+        <View className="flex-row items-center justify-between px-8 mt-8">
+          {menuItems.map((item) => (
+            <MenuContainer
+              key={item.key}
+              title={item.title}
+              imageSrc={item.imageSrc}
+              type={type}
+              setType={setType}
+            />
+          ))}
+        </View>
+        <View>
+          <View className="flex-row items-center justify-between px-4 mt-8">
+            <Text className="text-[#67C6E3] text-[28px] font-bold">Top List</Text>
+            <TouchableOpacity className="flex-row items-center justify-center space-x-2">
+              <Text className="text-[#67C6E3] text-[28px] font-bold">Explore</Text>
+              <FontAwesome name="long-arrow-right" size={24} color="#67C6E3" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
       
     
