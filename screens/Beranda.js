6c30a0792142b88d4e59a8883495612a92f60b16
regitapramesti1 <cahyaniregita623@gmail.com>
2024-05-21 13:20:45 +0700

    add line in Beranda.js

--- screens/Beranda.js
+++ screens/Beranda.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { Avatar } from '../assets';
 
 const Beranda = () => {
   const navigation = useNavigation();
@@ -15,10 +16,11 @@ const Beranda = () => {
     <SafeAreaView className="flex-1 bg-white relative">
       <View className="flex-row px-6 mt-8 items-center justify-between px-8">
       <View className="flex-row px-3 mt-6 items-center space-x-1">
-                <Text className="text-[#67C6E3] text-5xl font-bold">News</Text>
+                <Text className="text-[#67C6E3] text-3xl font-bold">News</Text>
             <View>
-                <Text className="text-black text-5xl font-bold">Nexus</Text>
+                <Text className="text-black text-3xl font-bold">Nexus</Text>
             </View>
+
         </View>
       </View>
     </SafeAreaView>
