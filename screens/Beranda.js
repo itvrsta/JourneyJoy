e5ee5483159b7ea819b9b4d7bacf6a3897d72f9b
regitapramesti1 <cahyaniregita623@gmail.com>
2024-05-23 17:57:21 +0700

    Change line in Beranda.js, change in HomeScreen.js, change line in index.js

--- screens/Beranda.js
+++ screens/Beranda.js
@@ -1,8 +1,8 @@
 import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import MenuContainer from '../components/MenuContainer';
-import { all, sports, health } from '../assets';
+import { beach, mountain, museum } from '../assets';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 
@@ -18,9 +18,9 @@ const Beranda = () => {
           },[]);
 
           const menuItems = [
-            { key: "all", title: "all", imageSrc: all },
-            { key: "sports", title: "sports", imageSrc: sports },
-            { key: "health", title: "health", imageSrc: health }
+            { key: "beach", title: "Beach", imageSrc: beach },
+            { key: "mountain", title: "Mountain", imageSrc: mountain },
+            { key: "museum", title: "Museum", imageSrc: museum }
           ];
 
   return (
@@ -29,7 +29,7 @@ const Beranda = () => {
       <View className="flex-row px-3 mt-6 items-center space-x-1">
                 <Text className="text-[#67C6E3] text-3xl font-bold">News</Text>
             <View>
-                <Text className="text-black text-3xl font-bold">Nexus</Text>
+                <Text className="text-[#3C5B6F] text-3xl font-bold">Nexus</Text>
             </View>
 
         </View>
@@ -53,10 +53,10 @@ const Beranda = () => {
         </View>
         <View>
           <View className="flex-row items-center justify-between px-4 mt-8">
-            <Text className="text-[#67C6E3] text-[28px] font-bold">Top List</Text>
+            <Text className="text-[#3C5B6F] text-[18px] font-bold">Top List</Text>
             <TouchableOpacity className="flex-row items-center justify-center space-x-2">
-              <Text className="text-[#67C6E3] text-[28px] font-bold">Explore</Text>
-              <FontAwesome name="long-arrow-right" size={24} color="#67C6E3" />
+              <Text className="text-[#3C5B6F] text-[18px] font-bold">Explore</Text>
+              <FontAwesome name="long-arrow-right" size={24} color="#3C5B6F" />
             </TouchableOpacity>
           </View>
         </View>
