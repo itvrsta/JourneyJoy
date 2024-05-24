53dd85eb5485690615925ea6269ed8c524806501
Vriesta <vriesta.ita888@gmail.com>
2024-05-25 04:21:36 +0700

    done

--- components/ItemCardContainer.js
+++ components/ItemCardContainer.js
@@ -1,19 +1,34 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
-import { Fontisto } from '@expo/vector-icons';
 import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { FontAwesome, Fontisto } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-const ItemCardContainer = ({ imageSrc, title, location }) => {
+const ItemCardContainer = ({ imageSrc, title, location, description }) => {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[182px] my-2">
+          <TouchableOpacity 
+        onPress={() => navigation.navigate("ItemScreen", { name: title, photo: imageSrc, description: description })}
+        className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[175px] my-2"
+      >
       <Image
         source={{ uri: imageSrc }}  
-        className="w-full h-40 rounded-md object-cover"
+        className="w-full h-[150px] rounded-md object-cover"
       />
-      <Text className="text-[#3C5B6F] text-[18px] font-bold">{title}</Text>
+
+      {title ? (
+        <>
+      <Text className="text-[#3C5B6F] text-[18px] font-bold">{title}
+      </Text>
+
       <View className="flex-row items-center space-x-1">
-        <Fontisto name="map-marker-alt" size={18} color="#3C5B6F" />
-        <Text className="text-[#428288] text-[15px] font-bold">{location}</Text>
+        <FontAwesome name="map-marker" size={18} color="#3C5B6F" />
+        <Text className="text-[#428288] text-[15px] font-bold">{location}
+          </Text>
       </View>
+      </>
+      ) : (
+        <></>
+      )}
     </TouchableOpacity>
   );
 };
---
+++ screens/ItemScreen.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { useLayoutEffect, useState, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const ItemScreen = () => {
+  const route = useRoute();
+  const { name, photo, description } = route.params;
+  const navigation = useNavigation();
+  const [placeData, setPlaceData] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
+
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+  <View className="flex-row px-6 mt-8 items-center justify-between">
+    <View className="flex-row px-3 mt-6 items-center space-x-1">
+    <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="w-10 h-10 rounded-full items-center justify-center bg-[#3C5B6F] shadow-md"
+        >
+          <FontAwesome5 name="chevron-left" size={24} color="#06b2be" />
+        </TouchableOpacity>
+      <Text className="text-[#67C6E3] text-3xl font-bold"> Journey</Text>
+      <Text className="text-[#3C5B6F] text-3xl font-bold">Joy</Text>
+    </View>
+  </View>
+
+    <ScrollView className="flex-1 px-4 py-6">
+    <View className="relative bg-white shadow-lg rounded-xl">
+      <Image
+        source={{ uri: photo }}
+        className="w-full h-52 rounded-lg"
+      />
+
+        <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
+      </View>
+    </View>
+
+      <View className="mt-4">
+        <Text className="text-lg font-bold text-gray-800">{name}</Text>
+        <Text className="text-gray-600 mt-2 text-justify">{description}</Text>
+      </View>
+
+  </ScrollView>
+</SafeAreaView>
+
+  );
+};
+
+
+export default ItemScreen;
